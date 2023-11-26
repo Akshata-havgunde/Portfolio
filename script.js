e75f57b622da1e2e3992504dce8a4e843754cb57
Akshata-havgunde <112488039+Akshata-havgunde@users.noreply.github.com>
2023-11-26 20:21:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    
+    if(top >= offset && top < offset + height){
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      });
+    };
+  });
+};
+
+//typing annimation
+var typed = new Typed('.text', {
+    strings: ["Frontend developer" , "Web Developer" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+//Skills section
+
+const block = document.querySelectorAll('.block');
+window.addEventListener('load', function(){
+  block.forEach(item => {
+    let numElement = item.querySelector('.num');
+    let num = parseInt(numElement.innerText);
+    let count = 0;
+    let time = 2000 / num;
+    let circle = item.querySelector('.circle');
+    setInterval(() => {
+      if(count == num){
+        clearInterval();
+      } else {
+        count += 1;
+        numElement.innerText = count;
+      }
+    }, time)
+    circle.style.strokeDashoffset 
+      = 503 - ( 503 * ( num / 100 ));
+    let dots = item.querySelector('.dots');
+    dots.style.transform = 
+      `rotate(${360 * (num / 100)}deg)`;
+    if(num == 100){
+      dots.style.opacity = 0;
+    }
+  })
+});
+
+//responsive header
+
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
